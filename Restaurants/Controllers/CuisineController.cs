45daa5e49ba67c06f7663b7387b4c17c9a483d6e
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-17 15:09:24 -0700

    Add path to add cuisine thereby allowing us to finally add a restaurant for god's sake!
    Co-Authored-By Hernan Verar <hernanverar@gmail.com>

--- Restaurants/Controllers/CuisineController.cs
+++ Restaurants/Controllers/CuisineController.cs
@@ -15,6 +15,22 @@ namespace Restaurants.Controllers
     {
       _db = db;
     }
+    public ActionResult Index()
+    {
+        List<Cuisine> model = _db.Cuisine.ToList();
+      return View(model);
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Cuisine cuisine)
+    {
+        _db.Cuisine.Add(cuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
     public ActionResult Details(int id)
     {
       Cuisine thisCuisine = _db.Cuisine
