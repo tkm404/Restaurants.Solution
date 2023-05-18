d8bfceb9d8b233a23659afd8b07d8ca2649852a4
Thomas McDowell <tmcdowellddhs@yahoo.com>
2023-05-18 08:57:54 -0700

    add dump file

---
+++ Restaurant.sql
@@ -0,0 +1,77 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: Restaurants
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Cuisine`
+--
+
+DROP TABLE IF EXISTS `Cuisine`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Cuisine` (
+  `CuisineId` int NOT NULL AUTO_INCREMENT,
+  `Type` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`CuisineId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Cuisine`
+--
+
+LOCK TABLES `Cuisine` WRITE;
+/*!40000 ALTER TABLE `Cuisine` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Cuisine` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Restaurant`
+--
+
+DROP TABLE IF EXISTS `Restaurant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Restaurant` (
+  `RestaurantId` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) DEFAULT NULL,
+  `PriceTier` varchar(50) DEFAULT NULL,
+  `Style` varchar(50) DEFAULT NULL,
+  `Smoking` tinyint DEFAULT NULL,
+  `Alcohol` tinyint DEFAULT NULL,
+  PRIMARY KEY (`RestaurantId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Restaurant`
+--
+
+LOCK TABLES `Restaurant` WRITE;
+/*!40000 ALTER TABLE `Restaurant` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Restaurant` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-05-18  8:57:09
